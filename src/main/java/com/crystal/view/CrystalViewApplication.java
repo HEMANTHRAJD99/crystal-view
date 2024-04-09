dd34b3be685084cd5161c02079f50c2a74822a52
Hemanth <hemanthrajd1999@gmail.com>
2024-04-09 23:11:16 +0530

    added project

---
+++ src/main/java/com/crystal/view/CrystalViewApplication.java
@@ -0,0 +1,13 @@
+package com.crystal.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrystalViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crystal/view/CrystalViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crystal.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
